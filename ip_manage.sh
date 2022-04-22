89153fa465dcb6d0e70e15eac02a7b95b919f843
marsbasex <95111233+marsbasex@users.noreply.github.com>
2022-04-22 18:28:44 +0800

    add autostatic & static & dhcp

--- ip_manage.sh
+++ ip_manage.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
 
 # prepare commands
-apt-get install -y curl net-tools
+if ! command -v curl &> /dev/null
+then
+    echo "curl could not be found"
+    apt-get install -y curl
+fi
 
-# get
-# static
-# recover
+if ! command -v route &> /dev/null
+then
+    echo "route could not be found"
+    apt-get install -y net-tools
+fi
 
-File="/etc/netplan/88-ip-config.yaml"
+StaticFile="/etc/netplan/88-ip-config.yaml"
+DHCPConfigFile="/etc/netplan/77-dhcp-config.yaml"
+
+function valid_ip() {
+    local  ip=$1
+    local  stat=1
+
+    if [[ $ip =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]; then
+        OIFS=$IFS
+        IFS='.'
+        ip=($ip)
+        IFS=$OIFS
+        [[ ${ip[0]} -le 255 && ${ip[1]} -le 255 \
+            && ${ip[2]} -le 255 && ${ip[3]} -le 255 ]]
+        stat=$?
+    fi
+    if [ $stat != 0 ]; then
+        echo "IP $1 不合法"
+        exit -1
+    fi
+}
 
 #Check NetWork
 function network()
@@ -28,9 +54,7 @@ function get() {
     netplan get
 }
 
-function static() {
-    # echo run static $1
-
+function autostatic() {
     # check network
     network
     if [ $? -eq 0 ];then
@@ -43,6 +67,12 @@ function static() {
         echo "获取IP出错"
         exit -1
     fi
+    tmps=(${IPAndMASK//\// })
+    valid_ip ${tmps[0]}
+    if [ ${tmps[1]} = "" ]; then
+        echo "无效的ip/mask $IPAndMASK"
+        exit -1
+    fi
     Name=`route | grep 'default' | awk '{print $8}'`
     if [ "$Name" = "" ]; then
         echo "获取网卡出错"
@@ -53,12 +83,13 @@ function static() {
         echo "获取GATEWAY出错"
         exit -1
     fi
+    valid_ip $GATEWAY
 
-    if [  -f ${File} ];then
+    if [  -f ${StaticFile} ];then
         echo "IP曾固定过，具体如下："
     else
         echo "IP固定成功："
-        cat > $File <<EOF
+        cat > $StaticFile <<EOF
 network:
     version: 2
     ethernets:
@@ -77,9 +108,81 @@ EOF
     get
 }
 
+function static() {
+    # check network
+    network
+    if [ $? -eq 0 ];then
+        echo "请检查您的网络，服务器无网络"
+        exit -1
+    fi
+
+    usage="Usage: static <ip/mask> <gateway> <dns>"
+
+    IPAndMASK=$1
+    if [ "$IPAndMASK" = "" ]; then
+        echo "获取IP出错"
+        echo $usage
+        exit -1
+    fi
+    tmps=(${IPAndMASK//\// })
+    valid_ip ${tmps[0]}
+    if [[ ${tmps[1]} = "" ]]; then
+        echo "无效的ip/mask $IPAndMASK"
+        exit -1
+    fi
+
+    GATEWAY=$2
+    if [ "$GATEWAY" = "" ]; then
+        echo "获取GATEWAY出错"
+        echo $usage
+        exit -1
+    fi
+    valid_ip $GATEWAY
+
+    DNS=$3
+    if [ "$DNS" = "" ]; then
+        echo "获取DNS出错"
+        echo $usage
+        exit -1
+    fi
+    dnslines=""
+    for v in ${DNS//,/ }
+    do
+        valid_ip $v
+        dnslines="$dnslines                    - $v\n"
+    done
+
+    Name=`route | grep 'default' | awk '{print $8}'`
+    if [ "$Name" = "" ]; then
+        echo "获取网卡出错"
+        exit -1
+    fi
+
+    if [  -f ${StaticFile} ];then
+        echo "IP曾固定过，具体如下："
+    else
+        echo "IP固定成功："
+        cat > $StaticFile <<EOF
+network:
+    version: 2
+    ethernets:
+        $Name:
+            addresses:
+                - $IPAndMASK
+            dhcp4: false
+            gateway4: $GATEWAY
+            nameservers:
+                addresses:
+`echo -e "$dnslines"`
+EOF
+    fi
+    netplan apply
+    get
+}
+
 function recover() {
-    if [  -f ${File} ];then
-        rm -f $File
+    if [  -f ${StaticFile} ];then
+        rm -f $StaticFile
         netplan apply
         echo "IP配置已恢复"
     else
@@ -88,23 +191,52 @@ function recover() {
     get
 }
 
+function backallconfig() {
+    backupdir=/etc/netplan/`date '+%Y%m%d_%H%M%S'`
+    mkdir $backupdir
+    mv /etc/netplan/*.yaml $backupdir
+}
+
+function dhcp() {
+    Name=`route | grep 'default' | awk '{print $8}'`
+    if [ "$Name" = "" ]; then
+        echo "获取网卡出错"
+        exit -1
+    fi
+
+    backallconfig
+    echo "IP固定成功："
+    cat > $DHCPConfigFile <<EOF
+network:
+    version: 2
+    ethernets:
+        $Name:
+            dhcp4: true
+EOF
+    netplan apply
+    get
+}
+
 function help() {
     echo "Usage: $0 <command>"
     echo "commands:"
     echo "    get    : list current ip config"
-    echo "    static : Fix ip config"
+    echo "    autostatic : Fix ip config"
     echo "    recover: recover ip config"
+    echo "    static: fix ip with specified IP and gateway"
+    echo "    dhcp: remove all config and enable dhcp"
 }
 
 function main() {
     cmd=$1
+    shift
     if [ "$cmd" = "" ]; then
         help
         exit -1
     fi
-    # echo start $cmd $arg
-    $cmd
+    $cmd $*
 }
 
 # command arg
-main $1 $2
+#    static IPAndMASK GATEWAY dns
+main $*
